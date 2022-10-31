f14fd27e8759d6324896d88fb722248e07fdc299
Juergen Gmeiner <spamless.juergen@gmail.com>
2022-10-31 21:52:50 +0100

    nicer subscriptions

--- src/grmble/lyakf/frontend/subscriptions.cljs
+++ src/grmble/lyakf/frontend/subscriptions.cljs
@@ -2,11 +2,5 @@
   (:require
    [re-frame.core :as rf]))
 
-(rf/reg-sub :ui
-            (fn [db [& args]]
-              (pr ":ui" args)
-              (get-in db args)))
-(rf/reg-sub :config
-            (fn [db [& args]]
-              (pr ":config" args)
-              (get-in db args)))
+(rf/reg-sub :ui get-in)
+(rf/reg-sub :config get-in)
