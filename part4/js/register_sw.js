35e0462a0a75e500108a2c09809b58fa4fae9d0a
Juergen Gmeiner <spamless.juergen@gmail.com>
2022-10-31 21:23:33 +0100

    pwa ...

--- part4/js/register_sw.js
+++ part4/js/register_sw.js
@@ -3,15 +3,15 @@ const basePath = '/learn-you-a-keeframe/part4'
 const registerServiceWorker = async () => {
     if ('serviceWorker' in navigator) {
         try {
-            const registration = await navigator.serviceWorker.register(basePath + '/js/sw.js', {
+            const registration = await navigator.serviceWorker.register(basePath + '/sw.js', {})
+            registration.addEventListener('updatefound', event => {
+                const newSW = registration.installing;
+                newSW.addEventListener('statechange', event => {
+                    if (newSW.state == 'installed') {
+                        console.log('new service worker installed')
+                    }
+                })
             })
-            if (registration.installing) {
-                console.log('Service worker installing')
-            } else if (registration.waiting) {
-                console.log('Service worker installed')
-            } else if (registration.active) {
-                console.log('Service worker active')
-            }
         } catch (error) {
             console.error(`Registration failed with ${error}`)
         }
